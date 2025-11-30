20cf983f7a5c5bb407cca8410f36244e0ed139c3
Pluggito <pluggitoshfl5050@gmail.com>
2025-11-30 14:36:36 +0100

    new branch

--- src/app/dashboard/client-wrapper.tsx
+++ src/app/dashboard/client-wrapper.tsx
@@ -13,7 +13,6 @@ export function DashboardClient() {
     <div className="container mx-auto px-4 md:px-6 space-y-6 pt-20 pb-24 md:pb-8">
       <DashboardHeader />
       <StatsCards />
-
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
           <ActiveGoals />
